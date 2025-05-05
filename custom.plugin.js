dc80f305efbf8d9e056b828b17d3ec0beec74f05
ministrgolub <ministrgolub@gmail.com>
2025-05-05 15:42:32 +0500

    Update custom.plugin.js

--- custom.plugin.js
+++ custom.plugin.js
@@ -1,41 +1,54 @@
-const { Plugin } = require('powercord/entities');
+/**
+ * @name MyOwnPlugin
+ * @version 0.1
+ * @description Автоматически меняет кастомный статус
+ * @author ministrgolub
+ * @source https://github.com/ministrgolub/ministrgolub
+ * @updateUrl https://github.com/ministrgolub/ministrgolub/blob/main/custom.plugin.js
+ */
 
-module.exports = class AutoStatusChanger extends Plugin {
-    startPlugin() {
+module.exports = class MyOwnPlugin {
+    start() {
         this.statuses = [
-            "Привет, tretiyizdvuh.aic"
-            "tretiyizdvuh.aic- Здравия желаю, пользователь"
+            "Привет, tretiyizdvuh.aic",
+            "tretiyizdvuh.aic - Здравия желаю, пользователь",
             "Получить доступ к файлам 715",
-            "Получение доступа к файлам 715...",
+            "tretiyizdvuh.aic - Получение доступа к файлам 715...",
             "...",
-            "Неудача, повторите попытку позже...",
+            "tretiyizdvuh.aic - Неудача, повторите попытку позже...",
             "...",
             "....",
             ".....",
-            "Ещё одна попытка разблокирована...",
-            "...",
-            // Добавьте свои статусы
+            "tretiyizdvuh.aic - Ещё одна попытка разблокирована...",
+            "..."
         ];
         this.index = 0;
-
-        this.changeStatus();
-        setInterval(() => this.changeStatus(), 10000); // Каждые 10 секунд
+        this.setStatus();
+        this.interval = setInterval(() => this.setStatus(), 10000); // Каждые 10 секунд
     }
 
-    changeStatus() {
+    setStatus() {
         const status = this.statuses[this.index];
         this.index = (this.index + 1) % this.statuses.length;
 
-        // Изменение статуса через API Discord
-        const { getCurrentUser } = require('discord-webhooks');
-        const user = getCurrentUser();
-        
-        if (user) {
-            user.setActivity(status);
+        const { WebpackModules, DiscordModules } = BdApi;
+        const UserStatusStore = WebpackModules.getByProps("updateCustomStatus");
+
+        if (UserStatusStore) {
+            UserStatusStore.updateCustomStatus({
+                text: status,
+                expires_at: null
+            });
         }
     }
 
-    pluginWillUnload() {
+    stop() {
         clearInterval(this.interval);
+        const { WebpackModules } = BdApi;
+        const UserStatusStore = WebpackModules.getByProps("updateCustomStatus");
+
+        if (UserStatusStore) {
+            UserStatusStore.updateCustomStatus(null); // Сброс статуса при выгрузке
+        }
     }
 };
